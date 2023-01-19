c52b10ab10574d9af08475d4ff203bdf0d2b503e
MewGame <119583876+MewGame@users.noreply.github.com>
2023-01-19 09:15:08 +0100

    Add files via upload

--- Projet prise en main  pyxel.py
+++ Projet prise en main  pyxel.py
@@ -1,34 +1,12 @@
-#Mahé de Berranger 1G2
+#Mahé de Berranger 1ere2
 #Samuel Brones 1G1
 #Herault Adel 1G1
 #kiyan Laranjeira 1G1
-import pyxel,random
-global x,y,dash,var_nuage,coord,coord_x,skin
+import pyxel, random
+global x,y,dash
 x=5
 y=44
 dash=2
-var_nuage=0
-coord=0
-coord_x=6
-skin=0
-def sol():
-    for sol in range(0,300,16):
-        pyxel.blt(sol,49,1,0,0,16,16,0)
-        pyxel.blt(sol,65,2,0,0,16,16,0)
-        pyxel.blt(32,32,1,32,0,16,16,0)
-            
-def nuage(y):
-    global coord,coord_x
-    pyxel.cls(12)
-    if coord>=150:
-        coord=0
-    coord=coord+1
-    if coord_x>=150:
-        coord_x=0
-    coord_x=coord_x+2
-    pyxel.blt(coord,y,1,48,0,16,16,0)
-    pyxel.blt(coord_x,y+8,1,48,0,16,16,0)
-        
 class App:
     def __init__(self):
         #initialisation de la fenêtre:dimension 160*80 et titre
@@ -41,52 +19,56 @@ class App:
         pyxel.playm(0,1,True)
         #Music made by Adel Herault (LE DJ)
         pyxel.run(self.update, self.draw)
-        
 
     def update(self):
-        global x,y,dash,skin
-        if pyxel.btnp(pyxel.KEY_Q):
+        global x,y,dash
+        if pyxel.btnp(pyxel.KEY_A):
             #On quitte si la touche Q (Qwerty) est pressee
             pyxel.quit()
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            skin=0
+        if pyxel.btn(pyxel.KEY_D):
             x=x+2
             if x>=156:
                 x=-2
                 dash=2
-        if pyxel.btn(pyxel.KEY_LEFT):
-            skin=16
+        if pyxel.btn(pyxel.KEY_Q):
             x=x-2
             if x<=-2:
                 x=-2
-        if pyxel.btn(pyxel.KEY_UP):
+        if pyxel.btn(pyxel.KEY_Z):
             y=y-2
-        if pyxel.btn(pyxel.KEY_DOWN):
-            if pyxel.pget(x+8, y+16)!=11:
-                y=y+2
-        
-                
+        if pyxel.btn(pyxel.KEY_S):
+            y=y+2
+            if y>=44:
+                y=44
         if pyxel.btnp(pyxel.KEY_SPACE):
+            pass
+        if pyxel.btnp(pyxel.KEY_1):
             if dash >= 1:
                 x=x+30
                 if x>=156:
                     x=-20
                     dash=2
             dash=dash-1
-        if pyxel.btnp(pyxel.KEY_P):
-            pyxel.blt(x,y,1,0,0,16,16,0)
-        
-        
+            
             
             
     def draw(self):
-        global skin
-        nuage(8)
-        sol()
-        pyxel.blt(x,y,0,skin,0,16,16,0)
-        
-            
-        
+        pyxel.cls(12)
+        pyxel.blt(150,15,1,32,0,16,16,0)
+        pyxel.blt(130,20,1,32,0,16,16,0)
+        pyxel.blt(110,15,1,32,0,16,16,0)
+        pyxel.blt(90,25,1,32,0,16,16,0)
+        pyxel.blt(70,15,1,32,0,16,16,0)
+        pyxel.blt(50,20,1,32,0,16,16,0)
+        pyxel.blt(30,15,1,32,0,16,16,0)
+        pyxel.blt(10,25,1,32,0,16,16,0)
+        pyxel.blt(-7,15,1,32,0,16,16,0)
+        pyxel.blt(60,25,1,16,0,16,16,0)
+        pyxel.blt(100,10,1,16,0,16,16,0)
+        for sol in range(0,300,16):
+            pyxel.blt(sol,47,1,8,16,16,16,0)
+            pyxel.blt(sol,63,2,0,0,16,16,0)
+        pyxel.blt(x,y,0,0,0,16,16,0)
         
         
         
