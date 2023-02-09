dfcb5aa02961e4915adf92e33861ebef8a04f8c8
PowerMah7 <119583921+PowerMah7@users.noreply.github.com>
2023-02-09 11:07:11 +0100

    Add files via upload

--- Projet prise en main  pyxel.py
+++ Projet prise en main  pyxel.py
@@ -3,7 +3,7 @@
 #Herault Adel 1G1
 #kiyan Laranjeira 1G1
 import pyxel,random
-global var_nuage,coord,coord_x,skin,map_x,map_y,sol_color,map_id
+global var_nuage,coord,coord_x,skin,map_x,map_y,sol_color,map_id,Saut
 map_id=0
 map_y=0
 map_x=0
@@ -12,7 +12,7 @@ var_nuage=0
 coord=0
 coord_x=6
 skin=0
-
+Saut=False
 def sol():
     for sol in range(0,300,16):
         pyxel.blt(sol,49,1,0,0,16,16,0)
@@ -54,7 +54,7 @@ class App:
         
 
     def update(self):
-        global x,y,dash,skin,map_x,map_id
+        global x,y,dash,skin,map_x,map_id,Saut
         if pyxel.btnp(pyxel.KEY_Q):
             #On quitte si la touche Q (Qwerty) est pressee
             pyxel.quit()
@@ -68,34 +68,39 @@ class App:
                 dash=2
         if pyxel.btn(pyxel.KEY_LEFT):
             skin=16
-            self.player_x=self.player_x-self.player_speed
-            if self.player_x<=5:
-                self.player_x=5
+            self.player_x-=self.player_speed
+            if self.player_x<=2:
+                self.player_x=2
                 if map_x!=0:
                     map_id -= 1
                     map_x=map_x-160
-                    self.player_x=150
-                    self.player_y=42
+                    self.player_x=145
                     
-        if pyxel.btn(pyxel.KEY_UP) and self.player_y == 43:
+        if pyxel.btn(pyxel.KEY_UP) and Saut==True:
             self.jump_speed = -5
-            
+            Saut=False
         self.jump_speed += self.gravity
         self.player_y += self.jump_speed
         
-        
-        if pyxel.pget(self.player_x + 8, self.player_y + 16) == 11:
-            self.jump_speed = 0
-            self.gravity = 0
-        if pyxel.pget(self.player_x + 8, self.player_y + 16) != 11:
-            self.gravity = 0.5
+        for i in range(6,10):
+            if pyxel.pget(self.player_x + i, self.player_y + 16) == 11 or pyxel.pget(self.player_x + i, self.player_y + 16)==9:
+                self.jump_speed = 0
+                self.gravity = 0
+                self.player_y=int(self.player_y)
+                Saut=True
+            if pyxel.pget(self.player_x + i, self.player_y + 16) != 11 and pyxel.pget(self.player_x + i, self.player_y + 16)!=9:
+                self.gravity = 0.5
+                Saut=False
         
         if pyxel.btnp(pyxel.KEY_SPACE):
             if dash >= 1:
-                self.player_x=self.player_x+30
-                if self.player_x>=156:
-                    self.player_x=-20
-                    dash=2
+                if skin==0:
+                    self.player_x+=30
+                if skin==16:
+                    self.player_x-=30
+                    if self.player_x>=156:
+                        self.player_x=-20
+                        dash=2
             dash=dash-1
         if pyxel.btnp(pyxel.KEY_P):
             pyxel.blt(self.player_x,self.player_y,1,0,0,16,16,0)
