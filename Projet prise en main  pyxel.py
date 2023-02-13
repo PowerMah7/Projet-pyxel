8185b81ab3fba0de6bae1689d6393fb314ace2d8
PowerMah7 <119583921+PowerMah7@users.noreply.github.com>
2023-02-13 11:46:45 +0100

    Add files via upload

--- Projet prise en main  pyxel.py
+++ Projet prise en main  pyxel.py
@@ -3,7 +3,8 @@
 #Herault Adel 1G1
 #kiyan Laranjeira 1G1
 import pyxel,random
-global var_nuage,coord,coord_x,skin,map_x,map_y,sol_color,map_id,Saut
+global var_nuage,coord,coord_x,skin,map_x,map_y,sol_color,map_id,Saut,jumping
+jumping=0
 map_id=0
 map_y=0
 map_x=0
@@ -38,13 +39,13 @@ class App:
         pyxel.load("media.pyxres")
         #les methodes update() et draw() sont executables en boucles
         #30 fois par seconde
-        pyxel.music(0)
-        pyxel.playm(0,1,True)
+        pyxel.music(7)
+        pyxel.playm(7,1,True)
         self.player_x = 5
-        self.player_y = 40
+        self.player_y = 41
         self.player_speed = 2
         self.jump_speed = 0
-        self.gravity = 0.5
+        self.gravity = 0.4
         #Music made by Adel Herault (LE DJ)
         pyxel.run(self.update, self.draw)
        
@@ -54,20 +55,24 @@ class App:
         
 
     def update(self):
-        global x,y,dash,skin,map_x,map_id,Saut
+        global x,y,dash,skin,map_x,map_id,Saut,jumping
         if pyxel.btnp(pyxel.KEY_Q):
             #On quitte si la touche Q (Qwerty) est pressee
             pyxel.quit()
-        if pyxel.btn(pyxel.KEY_RIGHT):
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.player_y<=50:
             skin=0
+            if jumping==1:
+                skin=32
             self.player_x=self.player_x+self.player_speed
             if self.player_x>=150:
                 map_id += 1
                 self.player_x=5
                 map_x=map_x+160
                 dash=2
-        if pyxel.btn(pyxel.KEY_LEFT):
+        if pyxel.btn(pyxel.KEY_LEFT) and self.player_y<=50:
             skin=16
+            if jumping==1:
+                skin=48
             self.player_x-=self.player_speed
             if self.player_x<=2:
                 self.player_x=2
@@ -77,19 +82,21 @@ class App:
                     self.player_x=145
                     
         if pyxel.btn(pyxel.KEY_UP) and Saut==True:
-            self.jump_speed = -5
+            jumping=1
+            self.jump_speed = -4
             Saut=False
         self.jump_speed += self.gravity
         self.player_y += self.jump_speed
         
-        for i in range(6,10):
-            if pyxel.pget(self.player_x + i, self.player_y + 16) == 11 or pyxel.pget(self.player_x + i, self.player_y + 16)==9:
+        for i in range(6,9):
+            if pyxel.pget(self.player_x + i, self.player_y + 16) == 11 or pyxel.pget(self.player_x + i, self.player_y + 16)==9 or pyxel.pget(self.player_x + i, self.player_y + 16)==6:
+                jumping=0
                 self.jump_speed = 0
                 self.gravity = 0
-                self.player_y=int(self.player_y)
+                self.player_y=int(self.player_y)  
                 Saut=True
-            if pyxel.pget(self.player_x + i, self.player_y + 16) != 11 and pyxel.pget(self.player_x + i, self.player_y + 16)!=9:
-                self.gravity = 0.5
+            if pyxel.pget(self.player_x + i, self.player_y + 16) != 11 and pyxel.pget(self.player_x + i, self.player_y + 16)!=9 and pyxel.pget(self.player_x + i, self.player_y + 16)!=6:
+                self.gravity = 0.4
                 Saut=False
         
         if pyxel.btnp(pyxel.KEY_SPACE):
@@ -105,7 +112,7 @@ class App:
         if pyxel.btnp(pyxel.KEY_P):
             pyxel.blt(self.player_x,self.player_y,1,0,0,16,16,0)
             
-        if self.player_y>=100:
+        if self.player_y>=62:
             map_id=0
             map_y=0
             map_x=0
@@ -115,10 +122,11 @@ class App:
             coord_x=6
             skin=0
             self.player_x = 5
-            self.player_y = 40
+            self.player_y = 41
             self.player_speed = 2
             self.jump_speed = 0
-            self.gravity = 0.5
+            self.gravity = 0.4
+            jumping=0
 
 
         
